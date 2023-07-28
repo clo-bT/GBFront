627be0940bb5a8eae8035d030794177ed8d4cf97
박승희 <pshee0329@naver.com>
2023-07-28 17:53:36 +0900

    지도 구현 전, 라우터 완료

--- test/src/App.js
+++ test/src/App.js
@@ -5,7 +5,7 @@ import Gombangba from './component/Gombangba';
 import Zzimlist from './component/Zimlist';
 import Roomout from './component/Roomout';
 import Signuplogin from './component/Signuplogin';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
 import './App.css';
 
 const App = () => {
@@ -13,15 +13,14 @@ const App = () => {
     <BrowserRouter>
       <div className='App'>
         <Routes>
-          <Route path="/" element={<Header />}>
-            <Route path="/map" element={<Map />}></Route>
-            <Route path="/gombangba" element={<Gombangba />}></Route>
-            <Route path="/zzim" element={<Zzimlist />}></Route>
-            <Route path="/roomout" element={<Roomout />}></Route>
+          <Route path="/" element={<Header  />}>
+            <Route path="/map" element={<Map />} />
+            <Route path="/gombangba" element={<Gombangba />} />
+            <Route path="/zzim" element={<Zzimlist />} />
+            <Route path="/roomout" element={<Roomout />} />
+            <Route path="/login" element={<Signuplogin />} />
           </Route>
         </Routes>
-        
-        <Signuplogin />
       </div>
     </BrowserRouter>
   );
--- test/src/component/MapPage.js
+++ test/src/component/MapPage.js
@@ -1,11 +1,38 @@
 
+import React, { useEffect } from 'react';
+import { KakaoMap, Marker } from 'react-kakao-maps';
 
+const MapPage = () => {
+  useEffect(() => {
+    // 데이터를 가져와서 마커를 생성하고 클러스터러 객체에 넘겨줍니다.
+    // 해당 기능은 useEffect 내에서 수행하도록 합니다.
+    fetch('/download/web/data/chicken.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const markers = data.positions.map((position) => ({
+          lat: position.lat,
+          lng: position.lng,
+        }));
+        // markers를 상태로 관리하고, MapPage 컴포넌트를 렌더링할 때 사용할 수 있도록 합니다.
+        // 이후에 clusterer에 markers 배열을 전달하여 클러스터러에 마커들을 추가합니다.
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
-export default function MapPage() {
-    return (
-        <div className="map-page">
-            지도
-        </div>
-        
-    )
-}
+  return (
+    <div style={{ width: '100%', height: '500px' }}>
+      <KakaoMap
+        center={{ lat: 36.2683, lng: 127.6358 }}
+        level={14}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* 마커들을 이곳에 표시해주세요 */}
+        {/* <Marker ... /> */}
+      </KakaoMap>
+    </div>
+  );
+};
+
+export default MapPage;
