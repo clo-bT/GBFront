04802abd6f59dac9cc0f8f1d4a9f2d6e684fa00f
HyundoLee <hyundo1995@naver.com>
2023-08-01 10:31:54 +0900

    typo

--- test/src/components/GbbList.js
+++ test/src/components/GbbList.js
@@ -8,11 +8,11 @@ import { useLocation, useNavigate  } from 'react-router-dom';
         const images = location.state && location.state.images ? location.state.images : [];
     return (
         <>
-        <div class="background">
+        <div className="background">
         <Header/>
-                <h1 class="titlebar">당신의 곰방을 보여주세요</h1>
+                <h1 className="titlebar">당신의 곰방을 보여주세요</h1>
                 <button className='plus-button' onClick={() => navigate("/gbbcreate")}>+</button>
-            <div class="content">
+            <div className="content">
             {images.map((image, id) => (
                         <div className="imageContainer" key={id}>
                             <img src={image} alt={`${image}-${id}`} width={'200px'} />
