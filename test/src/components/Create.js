bded4516dc86ce20da82862bcbfd3513560c1f13
박승희 <pshee0329@naver.com>
2023-08-02 15:52:09 +0900

    해쉬중복 없애줘 현도 짱

--- test/src/components/Create.js
+++ test/src/components/Create.js
@@ -5,6 +5,8 @@ import Header from './Header';
 const Create = ({ onImageUpload }) => {
     const [selectedImages, setSelectedImages] = useState([]);
     const [hashtag, setHashtag] = useState('');
+    const [selectedHashtags, setSelectedHashtags] = useState([]);
+
         const navigate = useNavigate  ();
         
         const handleImageChange = (event) => {
@@ -14,10 +16,24 @@ const Create = ({ onImageUpload }) => {
     const handleHashtagChange = (event) => {
         setHashtag(event.target.value);
     };
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            const value = event.target.value.trim();
+            if (hashtag.trim() !== '' && !selectedHashtags.includes(value)) {
+                setSelectedHashtags([...selectedHashtags, '#'+hashtag]);
+                setHashtag('');
+            }else {
+                event.preventDefault(); // 기본 동작 방지하여 커서가 입력창에 남아있게 함
+                event.target.value = ''; // 이미 있는 해시태그나 유효하지 않은 입력일 경우 입력창 비우기
+            }
+        }
+    };
+    
+
     const handleUpload = () => {
-        if (selectedImages.length > 0 && hashtag.trim() !== '') {
+        if (selectedImages.length > 0 || hashtag.trim() !== '') {
         const newImageGroup = {
-            hashtag: hashtag,
+            hashtag: selectedHashtags,
             images: selectedImages,
         };
 
@@ -28,8 +44,6 @@ const Create = ({ onImageUpload }) => {
         setHashtag('');
         navigate('/gbblist');
         // navigate('/list', {setSelectedImages});
-        } else {
-        alert('사진과 해쉬태그를 입력해주세요');
         }
     };
 
@@ -46,9 +60,19 @@ const Create = ({ onImageUpload }) => {
         </div>
         <div>
             <label>해시태그:</label>
-            <input type="text" value={hashtag} onChange={handleHashtagChange} placeholder='# 해시태그를 입력하세요'/>
+                <input
+                    type="text"
+                    value={hashtag}
+                    onKeyDown={handleKeyDown}
+                    onChange={handleHashtagChange}
+                    placeholder='# 해시태그를 입력하세요'
+                />
             </div>
-            
+        <div>
+            {selectedHashtags.map((tag, index) => (
+                <span key={index}>{tag}</span>
+            ))}
+        </div>
         <button onClick={handleUpload}>Upload</button>
         </div>
     );
