f2ebc3eee1ec035f70017653ba4b598302f475f0
HyundoLee <hyundo1995@gmail.com>
2023-08-07 17:41:28 +0900

    signup

--- test/src/pages/Auth.js
+++ test/src/pages/Auth.js
@@ -3,37 +3,22 @@ import Header from '../components/Header';
 import axios from 'axios';
 
 export default function Auth() {
-    const [c, setcode] = useState('')    
-    const [accessToken,setaccessToken] =useState('')
-    const [userinfo,setuserpp] = useState([])
+    const [c, setcode] = useState('')
+    const [id, setid] = useState('')    
+    const [shownameform, setshownameform] = useState(false)    
+    const [value,iptvalue] = useState('')
     
-    
-    useEffect(()=>{
-        
         const gettoken = async ()=>{
             const code = new URL(window.location.href).searchParams.get("code");
             setcode(code)
-            const Rest_api_key='a20ef37212e1ae86b20e09630f6590ce' //REST API KEY
-            const data ={
-                grant_type: 'authorization_code',
-                client_id :  Rest_api_key,
-                redirect_uri :'http://localhost:3000/auth',
-                code
-            }
-            const headers = {
-                'Content-type' : 'application/x-www-form-urlencoded;charset=utf-8',
-            }
-            const queryString = Object.keys(data)
-            .map(k=>encodeURIComponent(k)+'='+encodeURIComponent(data[k]))
-            .join('&')
-            
             try{        
-                await axios.post("https://kauth.kakao.com/oauth/token",queryString
-                ,headers)
+                await axios.get(`http://localhost:8080/member/login?code=${code}`)
                 .then(function(r){
-                    console.log('tokkensuccess',r.data)
-                    setaccessToken(r.data.access_token)
-                    getUserInfo(r.data.access_token)
+                    console.log('success',r.data)
+                    if(r.data.code===200){
+                        setshownameform(true)
+                        setid(r.data.data.id)
+                    }
                 })
             }
             catch(error){
@@ -41,66 +26,51 @@ export default function Auth() {
             }
         }
 
-        const getUserInfo = async (at)=>{
-            const headers = {
-                'Authorization' : 'Bearer '+ at,
+        const postname = async (name) => {
+            const params = {
+                'id':id,
+                'name':name
+            }  
+            const config = {
+                headers: {
+                'Content-Type': 'application/json'
+                }
             }
-            console.log(headers)
-            // 엑세스 토큰 헤더에 담기
 
-            // 카카오 사용자 정보 조회
-            await axios.get("https://kapi.kakao.com/v2/user/me", {headers:headers})
-            .then(function(r){
-                console.log(r.data)
-                console.log(r.data.properties)
-                setuserpp(r.data)
-            })
-            .catch(function(error){console.error('error',error)})
+            try {
+                // const response = await axios.post('http://localhost:8080/member/update',null,{params});
+                const response = await axios.post('http://localhost:8080/member/update',params,config);
+                console.log('success2', response.data);
+            } catch (error) {
+                console.error('Error posting data:', error);
+            }
         }
-        gettoken()
 
-    },[])
+        function handleKeyPress(event){
+            if (event.key === "Enter" || event.keyCode === 13){
+                console.log(value)
+                postname(value)
+            }
+        }
     
     
 
     return(
         <div>
             <Header/>
+            <button onClick={gettoken}>12313</button>
             <div className="auth">
                 인가코드 :  {c}
+                {id? <div>uuid : {id}</div> :''}
             </div>
-            <div>
-                토큰정보 : {accessToken}
-            </div>
-            #properties
-            {userinfo ? (
-            <div>
-                <div>connected: {userinfo.connected_at}</div>
-                <div>channelID: {userinfo.id}</div>
-                <div>
-                kakao_account:
-                {userinfo.kakao_account
-                    ? Object.entries(userinfo.kakao_account).map(([key, value]) => (
-                        <div key={key}>
-                        {key}: {JSON.stringify(value)}
-                        </div>
-                    ))
-                    : ''}
-                </div>
-                <div>
-                properties:
-                {userinfo.properties
-                    ? Object.entries(userinfo.properties).map(([key, value]) => (
-                        <div key={key}>
-                        {key}: {JSON.stringify(value)}
-                        </div>
-                    ))
-                    : ''}
-                </div>
-            </div>
-            ) : (
-            ''
-            )}
+                {shownameform&&
+                <input 
+                type="text" 
+                placeholder='이름을 입력하시오' 
+                onKeyDown={handleKeyPress} 
+                onChange={(event)=>{iptvalue(event.target.value)}} 
+                />}
+            
         </div>
         
     )
