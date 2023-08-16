070038daaf8e0f791a6a9e26dcf314a26740b37b
HyundoLee <hyundo1995@gmail.com>
2023-08-17 02:56:27 +0900

    roomlistitems

--- test/src/pages/RoomDetail.js
+++ test/src/pages/RoomDetail.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const RoomDetail = () => {
     const [roomdata, setRoomData] = useState([]);
     const [roomoption, setRoomOption] = useState([]);
-    const roomid=1
+    const roomid = 3
     axios.get(`${process.env.REACT_APP_API_ROOT}/roomdeal/${roomid}`)
     .then(response => {
         console.log('받아온 정보 : ', response.data);
