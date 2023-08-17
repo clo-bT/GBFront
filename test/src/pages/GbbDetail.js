d33447cd2b310984d7d85d4b5e54c7c2aa1e1efe
박승희 <tael8213@gmail.com>
2023-08-17 19:50:22 +0900

    곰방봐

---
+++ test/src/pages/GbbDetail.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useState,useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+
+const GbbDetail = () => {
+    const [userid, setUserid] = useState("");
+    const { articleId } = useParams();
+    // showroom/{articleId}
+    useEffect(() => {
+        const member = JSON.parse(sessionStorage.getItem("member"));
+        const useruuid = member.id;
+        setUserid(useruuid);
+      }, [setUserid]);
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_ROOT}/showroom/{articleId}`
+        ).then((response) => {
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    })
+    return (
+        <div>여긴 방꾸 디테일</div>
+    )
+}
+export default GbbDetail;
